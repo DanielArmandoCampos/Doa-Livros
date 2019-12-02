68a601b73eafeb175c7a30569bbf0d8554190fc7
Daniel Armando Campos <55712713+DanielArmandoCampos@users.noreply.github.com>
2019-12-02 06:35:57 -0300

    Add files via upload
    * início do projeto
    * criação das classes com os métodos e atributos

---
+++ Caregoria.cs
@@ -0,0 +1,7 @@
+class Catedoria:Livro{
+  private string cat;
+  public Catedoria(string nome, string editora, string autor, int anoDePublicacao, string cat):base(nome, editora, autor, anoDePublicacao){
+    this.cat = cat;
+  }
+
+}
---
+++ Endereco.cs
@@ -0,0 +1,12 @@
+class Endereco{
+  private string rua, cidade, bairro, estado, pais;
+  private int numero;
+  public Endereco (string rua, string cidade, string bairro, string estado, string pais, int numero){
+    this.rua = rua;
+    this.cidade = cidade;
+    this.bairro = bairro;
+    this. estado = estado;
+    this.pais = pais;
+    this.numero = numero;
+  }
+}
