ce7f04ec0b1f67fdaef1903de7f6ba186940aeb9
Daniel Armando Campos <55712713+DanielArmandoCampos@users.noreply.github.com>
2019-12-05 19:45:19 -0300

    Delete Caregoria.cs
    deletado pastas desnecessárias

--- Caregoria.cs
+++
@@ -1,7 +0,0 @@
-class Catedoria:Livro{
-  private string cat;
-  public Catedoria(string nome, string editora, string autor, int anoDePublicacao, string cat):base(nome, editora, autor, anoDePublicacao){
-    this.cat = cat;
-  }
-
-}
